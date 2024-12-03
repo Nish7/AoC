8c4e79cb9c650b058302ddbeb4e1e3e16a60a9f9
Nishil <nishilkapadia17@gmail.com>
2024-12-03 00:48:52 -0500

    chore: refactor the regex

--- 2024/day3/day3.go
+++ 2024/day3/day3.go
@@ -12,8 +12,7 @@ var input string
 
 func main() {
 	// input := "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-	re := regexp.MustCompile(`mul\(\s*(\d+)\s*,\s*(\d+)\s*\)`)
-
+	re := regexp.MustCompile(`mul\(\s*(\d+)\s*,\s*(\d+)\)`)
 	matches := re.FindAllStringSubmatch(input, -1)
 
 	total := 0
