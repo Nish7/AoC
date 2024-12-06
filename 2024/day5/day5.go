e1650ebc4a31a48db867b3215fed7f93fc351c07
Nishil <nishilkapadia17@gmail.com>
2024-12-05 22:08:15 -0500

    day 5 done part 1 and 2

---
+++ 2024/day5/day5.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"math"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+//go:embed test.txt
+var test string
+
+//go:embed input.txt
+var input string
+
+func main() {
+	rules, updates := parseInput(input)
+	goodUpdates := checkUpdates(updates, rules)
+	midVal := calcMidValues(goodUpdates)
+	fmt.Println(midVal)
+}
+
+func calcMidValues(goodUpdates [][]int) int {
+	mid := 0
+	for _, u := range goodUpdates {
+		midI := int(math.Floor(float64((len(u) - 1) / 2)))
+		mid += u[midI]
+	}
+	return mid
+}
+
+func checkUpdates(updates [][]int, rules [][2]int) (goodUpdates [][]int) {
+	// for each updates; go thorugh each rule and find the index of both rules and if they exist, then compare left should be greater
+	for _, up := range updates {
+		isNoBeuno := false
+		for i := 0; i < len(rules); i++ {
+			r := rules[i]
+			uno := slices.Index(up, r[0])
+			secondo := slices.Index(up, r[1])
+			if uno != -1 && secondo != -1 {
+				if uno > secondo {
+					isNoBeuno = true
+					up[uno], up[secondo] = up[secondo], up[uno]
+					i = 0
+				}
+			}
+		}
+
+		if isNoBeuno {
+			goodUpdates = append(goodUpdates, up)
+		}
+	}
+
+	return
+}
+
+func getRulesMap(rules [][3]int) map[int][]int {
+	mp := make(map[int][]int)
+	for _, r := range rules {
+		mp[r[0]] = append(mp[r[0]], r[1])
+	}
+	return mp
+}
+
+func parseInput(st string) (edges [][2]int, orders [][]int) {
+	p2idx := 0
+	trimNewLine := strings.TrimSuffix(st, "\n")
+	lines := strings.Split(trimNewLine, "\n")
+
+	for i, l := range lines {
+		if l == "" {
+			p2idx = i
+			break
+		}
+		var n1, n2 int
+		fmt.Sscanf(l, "%d|%d", &n1, &n2)
+		edges = append(edges, [2]int{n1, n2})
+	}
+
+	for i := p2idx + 1; i < len(lines); i++ {
+		fields := strings.Split(lines[i], ",")
+
+		var flds []int
+		for _, f := range fields {
+			fi, _ := strconv.Atoi(f)
+			flds = append(flds, fi)
+		}
+		orders = append(orders, flds)
+	}
+	return
+}
