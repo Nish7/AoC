4704caeb191ad45444c0d80060be6c9278179d6d
Nishil <nishilkapadia17@gmail.com>
2024-12-23 15:55:16 +0530

    p2 done

--- 2024/day16/day16.go
+++ 2024/day16/day16.go
@@ -21,66 +21,123 @@ func main() {
 		for j := 0; j < len(input[0]); j++ {
 			if input[i][j] == "S" {
 				fmt.Println("Starting", i, j)
-				out := FindPath(i, j, input)
-				fmt.Println(out)
+				out, lenOp := FindPath(i, j, input)
+				fmt.Println(out, lenOp)
 				break
 			}
 		}
 	}
 }
 
-func FindPath(sx, sy int, board [][]string) int {
+var directions = []struct {
+	dx, dy int
+	dir    string
+}{
+	{1, 0, "S"},  // South
+	{-1, 0, "N"}, // North
+	{0, 1, "E"},  // East
+	{0, -1, "W"}, // West
+}
+
+type VisitedNode struct {
+	x   int
+	y   int
+	dir string
+}
+
+type QueueNode struct {
+	x, y, score int
+	dir         string
+	path        map[[2]int]bool
+}
+
+func FindPath(sx, sy int, board [][]string) (int, int) {
 	min_v := math.MaxInt64
-	seen := make(map[[2]int]bool)
-
-	directions := []struct {
-		dx, dy int
-		dir    string
-	}{
-		{1, 0, "S"},  // South
-		{-1, 0, "N"}, // North
-		{0, 1, "E"},  // East
-		{0, -1, "W"}, // West
-	}
-
-	queue := [][4]interface{}{{sx, sy, 1001, ""}}
-	seen[[2]int{sx, sy}] = true
+	seen := make(map[VisitedNode]int)
+	queue := []QueueNode{{sx, sy, 1000, "", map[[2]int]bool{{sx, sy}: true}}} // we can add all the
+	seen[VisitedNode{sx, sy, ""}] = 0
+	optimalNodes := make(map[[2]int]bool)
 
 	for len(queue) > 0 {
-		sort.SliceStable(queue, func(i, j int) bool {
-			return queue[i][2].(int) < queue[j][2].(int)
+
+		sort.SliceStable(queue, func(i, j int) bool { // dijkastra's portion
+			return queue[i].score < queue[j].score // ideally should you be using a heap
 		})
 
 		node := queue[0]
 		queue = queue[1:]
+		i, j, p, dir, path := node.x, node.y, node.score, node.dir, node.path
 
-		fmt.Println(node)
+		if board[i][j] == "E" {
+			if p <= min_v {
+				min_v = p
+				for k := range path {
+					optimalNodes[k] = true
+				}
+			}
+			continue
+		}
 
-		i, j, p, lastDir := node[0].(int), node[1].(int), node[2].(int), node[3].(string)
+		// time.Sleep(0 * time.Millisecond)
+		board[i][j] = "O"
+		// PrintBoard(board)
+		// fmt.Println(node)
 
 		for _, d := range directions {
 			points := p + 1
-
-			if lastDir != "" && d.dir != lastDir {
+			if dir != "" && d.dir != dir {
 				points += 1000
 			}
 
 			ni, nj := i+d.dx, j+d.dy
 
-			if ni >= 0 && nj >= 0 && ni < len(board) && nj < len(board[0]) && board[ni][nj] != "#" && !seen[[2]int{ni, nj}] {
-				if board[ni][nj] == "E" {
-					if p < min_v {
-						min_v = p
-					}
-					continue
-				}
-				seen[[2]int{ni, nj}] = true
-				queue = append(queue, [4]interface{}{ni, nj, points, d.dir})
+			if ni < 0 ||
+				nj < 0 ||
+				ni >= len(board) ||
+				nj >= len(board[0]) ||
+				points > min_v ||
+				board[ni][nj] == "#" {
+				continue
 			}
+
+			if v, ok := seen[VisitedNode{ni, nj, d.dir}]; ok && v < points {
+				continue
+			}
+
+			newPath := make(map[[2]int]bool)
+			for k := range path {
+				newPath[k] = true
+			}
+			newPath[[2]int{ni, nj}] = true
+			seen[VisitedNode{ni, nj, d.dir}] = points
+			queue = append(queue, QueueNode{ni, nj, points, d.dir, newPath})
+
 		}
 	}
 
-	return min_v
+	return min_v, len(optimalNodes)
+}
+
+func PrintBoard(input [][]string) {
+	fmt.Print("\x1b[2J\x1b[H")
+
+	var builder strings.Builder
+
+	for _, row := range input {
+		for _, c := range row {
+			switch c {
+			case "O":
+				builder.WriteString("\x1b[31m@\x1b[0m") // Red for '@'
+			case "#":
+				builder.WriteString("\x1b[32m" + c + "\x1b[0m") // Green for '[' and ']'
+			default:
+				builder.WriteString(c) // Default character
+			}
+		}
+		builder.WriteByte('\n') // Newline after each row
+	}
+
+	fmt.Print(builder.String())
 }
 
 func ParseInput(in string) [][]string {
